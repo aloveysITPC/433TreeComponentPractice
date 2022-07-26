d8dc74a017d2280c57413dba4d5b4539e06309c8
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-07-26 12:20:10 -0400

    attempt to fix

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-function handleChange() {}
-
-function inputText() {}
-function addItem() {}
+import React, { useState } from "react";
 
 function InputArea() {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
